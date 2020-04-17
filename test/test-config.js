e143b5302442d2b8f26d5b5a7600fc0ec836d3b3
Cosmin Popovici <hellocosmin@gmail.com>
2020-04-17 20:57:13 +0300

    test: update make:config tests

--- test/test-config.js
+++ test/test-config.js
@@ -1,5 +1,6 @@
 const test = require('ava')
 const fs = require('fs-extra')
+const execa = require('execa')
 const Config = require('../src/commands/make/config')
 
 test.beforeEach(t => {
@@ -14,7 +15,7 @@ test.afterEach.always(async t => {
 })
 
 test('it scaffolds a basic config', async t => {
-  await Config.scaffold('basic', {full: false})
+  await execa('node bin/maizzle make:config basic')
 
   t.context.file = 'config.basic.js'
   const config = require(`../${t.context.file}`)
@@ -24,7 +25,7 @@ test('it scaffolds a basic config', async t => {
 })
 
 test('it scaffolds a full config', async t => {
-  await Config.scaffold('full', {full: true})
+  await execa('node bin/maizzle make:config full -f')
 
   t.context.file = 'config.full.js'
   const config = require(`../${t.context.file}`)
@@ -34,8 +35,8 @@ test('it scaffolds a full config', async t => {
 })
 
 test('it does not overwrite existing files', async t => {
-  await Config.scaffold('production', {full: false})
-  await Config.scaffold('production', {full: true})
+  await execa('node bin/maizzle make:config production')
+  await execa('node bin/maizzle make:config production -f')
 
   t.context.file = 'config.production.js'
   const config = require(`../${t.context.file}`)
