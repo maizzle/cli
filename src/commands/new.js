cb0d7a590adc01916dce9379ed092a5cc078eb34
Cosmin Popovici <hellocosmin@gmail.com>
2020-04-18 17:13:22 +0300

    feat: user prompts for `maizzle new`

--- src/commands/new.js
+++ src/commands/new.js
@@ -2,14 +2,50 @@ const ora = require('ora')
 const path = require('path')
 const fs = require('fs-extra')
 const execa = require('execa')
+const chalk = require('chalk')
+const inquirer = require('inquirer')
 const {isGitURL} = require('../utils')
 
 module.exports.scaffold = async (repo, dir, cmd) => {
+  if (cmd.args.length === 0) {
+    await inquirer
+      .prompt([
+        {
+          name: 'folder',
+          message: 'Project directory name',
+          default: 'maizzle'
+        },
+        {
+          name: 'starter',
+          type: 'confirm',
+          message: 'Scaffold based on a starter?',
+          default: false
+        },
+        {
+          name: 'repo',
+          message: 'Starter Git repository URL',
+          default: 'https://github.com/maizzle/maizzle.git',
+          when: answers => answers.starter
+        },
+        {
+          name: 'dependencies',
+          type: 'confirm',
+          message: 'Install NPM dependencies?',
+          default: true
+        }
+      ])
+      .then(answers => {
+        repo = answers.repo
+        dir = answers.folder
+        cmd.deps = answers.dependencies
+      })
+  }
+
   repo = repo || 'https://github.com/maizzle/maizzle.git'
   dir = dir || path.parse(repo).name
 
   const dest = path.join(process.cwd(), dir)
-  const spinner = ora(`Crafting new Maizzle project in ${dest}...`).start()
+  let spinner = ora(`Crafting new Maizzle project in ${dest}...`).start()
 
   if (!isGitURL(repo)) {
     spinner.fail(`not a Git repository: ${repo}`)
@@ -18,18 +54,27 @@ module.exports.scaffold = async (repo, dir, cmd) => {
 
   execa('git', ['clone', repo, dir])
     .then(async () => {
+      spinner = spinner.stopAndPersist({symbol: `${chalk.green('√')}`, text: 'Cloned Git repository'})
       process.chdir(dest)
       await fs.remove('.git')
       await fs.remove('.github')
 
       if (cmd.deps) {
-        spinner.text = 'Project downloaded, installing NPM dependencies...'
+        spinner.start('Installing NPM dependencies')
 
         execa('npm', ['install'])
-          .then(() => spinner.succeed('Maizzle project initialized, go create awesome emails!'))
+          .then(() => {
+            return spinner
+              .stopAndPersist({symbol: `${chalk.green('√')}`, text: 'Installed NPM dependencies'})
+              .succeed('Maizzle project initialized')
+              .info(`Now \`cd ${dir}\` and start building your emails`)
+          })
           .catch(({stderr}) => spinner.fail(stderr))
       } else {
-        spinner.succeed(`Maizzle project initialized. Remember to install the dependencies by running \`cd ${dir}\` and then \`npm install\``)
+        return spinner
+          .stopAndPersist({symbol: `${chalk.green('√')}`, text: 'Installed NPM dependencies'})
+          .succeed('Maizzle project initialized')
+          .info(`Remember to install the dependencies by running \`cd ${dir}\` and then \`npm install\``)
       }
     })
     .catch(({stderr}) => spinner.fail(stderr))
