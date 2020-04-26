2f653a2d65d6fa5db12483c0cff230f120ce0c3f
Cosmin Popovici <hellocosmin@gmail.com>
2020-04-26 16:50:50 +0300

    test: fix make:template test

--- test/test-template.js
+++ test/test-template.js
@@ -1,7 +1,6 @@
 const test = require('ava')
 const fs = require('fs-extra')
 const execa = require('execa')
-const cli = require.resolve('../bin/maizzle')
 
 test.beforeEach(t => {
   t.context.folder = '_temp_' + Math.random().toString(36).slice(2, 9)
@@ -15,7 +14,7 @@ test.afterEach.always(async t => {
 })
 
 test('it scaffolds a template', async t => {
-  await execa(cli, ['make:template', 'template.html'])
+  await execa.command('node bin/maizzle make:template template.html')
 
   t.context.folder = 'src/templates'
   const file = `${t.context.folder}/template.html`
@@ -25,7 +24,7 @@ test('it scaffolds a template', async t => {
 })
 
 test('it scaffolds a layout in the specified directory', async t => {
-  await execa(cli, ['make:template', 'template.html', '-d', t.context.folder])
+  await execa.command(`node bin/maizzle make:template template.html -d ${t.context.folder}`)
 
   const file = `${t.context.folder}/template.html`
 
@@ -34,14 +33,14 @@ test('it scaffolds a layout in the specified directory', async t => {
 })
 
 test('it requires a file extension', async t => {
-  await execa(cli, ['make:template', 'template', '-d', t.context.folder])
+  await execa.command(`node bin/maizzle make:template template -d ${t.context.folder}`)
   t.false(fs.existsSync(`${t.context.folder}/example`))
 })
 
 test('it does not overwrite existing files', async t => {
-  await execa(cli, ['make:template', 'template.html', '-d', t.context.folder])
+  await execa.command(`node bin/maizzle make:template template.html -d ${t.context.folder}`)
   const mtimeMs = fs.statSync(`${t.context.folder}/template.html`).mtimeMs
-  await execa(cli, ['make:template', 'template.html', '-d', t.context.folder])
+  await execa.command(`node bin/maizzle make:template template.html -d ${t.context.folder}`)
 
   t.is(fs.statSync(`${t.context.folder}/template.html`).mtimeMs, mtimeMs)
 })
