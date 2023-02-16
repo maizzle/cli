9459af411787e738258c4009a481f43330cc0bd3
Cosmin Popovici <hellocosmin@gmail.com>
2023-02-16 18:44:52 +0200

    test: remove build test

--- test/test-build.js
+++ test/test-build.js
@@ -1,12 +1,6 @@
 const test = require('ava')
 const execa = require('execa')
 
-test('throws if not in project (build)', async t => {
-  await t.throwsAsync(async () => {
-    await execa.command('node bin/maizzle build')
-  }, {instanceOf: Error})
-})
-
 test('throws if not in project (serve)', async t => {
   await t.throwsAsync(async () => {
     await execa.command('node bin/maizzle serve')
