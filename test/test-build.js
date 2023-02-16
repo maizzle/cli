1c27a7aa0d1cf786e58f00a3d139c607776f2135
Cosmin Popovici <hellocosmin@gmail.com>
2023-02-16 17:46:25 +0200

    test: fix build tests

--- test/test-build.js
+++ test/test-build.js
@@ -2,13 +2,13 @@ const test = require('ava')
 const execa = require('execa')
 
 test('throws if not in project (build)', async t => {
-  const {stderr} = await execa.command('node bin/maizzle build')
-
-  t.truthy(stderr.includes('Error: Framework not found'))
+  await t.throwsAsync(async () => {
+    await execa.command('node bin/maizzle build')
+  }, {instanceOf: Error})
 })
 
 test('throws if not in project (serve)', async t => {
-  const {stderr} = await execa.command('node bin/maizzle serve')
-
-  t.truthy(stderr.includes('Error: Framework not found'))
+  await t.throwsAsync(async () => {
+    await execa.command('node bin/maizzle serve')
+  }, {instanceOf: Error})
 })
