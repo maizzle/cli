df3e7c9fe92963ace52817e279db77d4ad282fa1
Cosmin Popovici <hellocosmin@gmail.com>
2023-02-16 18:39:39 +0200

    try shelljs

--- test/test-build.js
+++ test/test-build.js
@@ -1,10 +1,9 @@
 const test = require('ava')
 const execa = require('execa')
+const shell = require('shelljs')
 
 test('throws if not in project (build)', async t => {
-  await t.throwsAsync(async () => {
-    await execa.command('node bin/maizzle build production')
-  }, {instanceOf: Error})
+  t.truthy(shell.exec('node bin/maizzle build'), undefined)
 })
 
 test('throws if not in project (serve)', async t => {
